f786aae761d3b9ed34e8c997a5e54c030f57fc93
ocilical <32717754+ocilical@users.noreply.github.com>
2022-12-01 10:38:14 -0500

    simplify very slightly

--- day1/part2.py
+++ day1/part2.py
@@ -1,3 +1,3 @@
 with open("input", "r") as f:
-    most = sum(list(sorted([sum([int(x or 0) for x in l.split("\n")]) for l in f.read().split("\n\n")], reverse=True))[:3])
+    most = sum(list(sorted([sum([int(x or 0) for x in l.split()]) for l in f.read().split("\n\n")], reverse=True))[:3])
     print(most)
