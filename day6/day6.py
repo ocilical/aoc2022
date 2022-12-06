504cb10a7f8bcce577e34d2c7cdf7a75e1ec71d3
ocilical <32717754+ocilical@users.noreply.github.com>
2022-12-06 08:42:26 -0500

    finish day 6

--- day6/day6.py
+++ day6/day6.py
@@ -4,5 +4,10 @@ inputfile.close()
 
 for i in range(0, len(signal) - 3):
     if len(set(signal[i:i+4])) == 4:
-        print(f"part 1: {i}")
+        print(f"part 1: {i + 4}")
+        break
+
+for i in range(0, len(signal) - 13):
+    if len(set(signal[i:i+14])) == 14:
+        print(f"part 2: {i + 14}")
         break
