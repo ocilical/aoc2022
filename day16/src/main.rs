8dadc8d5d019bde61c6a7924a4b0cac022ac7942
ocilical <32717754+ocilical@users.noreply.github.com>
2022-12-16 18:46:04 -0500

    part 2 kinda

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -48,8 +48,6 @@ fn explore(
     curr: &String,
     open: &mut HashSet<String>,
 ) -> i32 {
-    //println!("memo: {:?}\ntime: {}\ncurr: {}\nopen: {:?}", memo, time, curr, open);
-
     if time <= 0 {
         return 0;
     }
@@ -61,25 +59,117 @@ fn explore(
     let curr_valve = maze.get(curr).unwrap();
     let mut res = 0;
 
+    // without turning on valve
     for valve in curr_valve.tunnels.iter() {
         res = max(res, explore(memo, maze, time - 1, valve, open))
     }
+
     // with turning valve
     if !open.contains(curr) && curr_valve.flow > 0 {
         open.insert(curr.to_string());
         for valve in curr_valve.tunnels.iter() {
-            res = max(res, ((time - 1) * curr_valve.flow) + explore(memo, maze, time - 2, valve, open));
+            res = max(
+                res,
+                ((time - 1) * curr_valve.flow) + explore(memo, maze, time - 2, valve, open),
+            );
         }
         open.remove(curr);
     }
 
-    memo.insert((curr.to_string(), time, open.iter().sorted().join(",")), res);
-    // without turning on valve
+    memo.insert(
+        (curr.to_string(), time, open.iter().sorted().join(",")),
+        res,
+    );
+
+    res
+}
+
+fn part2(input: &str) -> i32 {
+    let maze = parse_maze(input);
+    let mut memo = HashMap::new();
+    let mut visited = HashSet::new();
+    elphsplore(
+        &mut memo,
+        &maze,
+        26,
+        &"AA".to_string(),
+        &"AA".to_string(),
+        &mut visited,
+    )
+}
+
+fn elphsplore(
+    memo: &mut HashMap<(String, String, i32, String), i32>,
+    maze: &HashMap<String, Valve>,
+    time: i32,
+    curr: &String,
+    elph: &String,
+    open: &mut HashSet<String>,
+) -> i32 {
+    //println!("{:?}", open.iter().sorted().join(","));
+
+    if time <= 0 {
+        return 0;
+    }
+
+    if let Some(ans) = memo.get(&(
+        curr.to_string(),
+        elph.to_string(),
+        time,
+        open.iter().sorted().join(","),
+    )) {
+        return *ans;
+    }
+
+    let curr_valve = maze.get(curr).unwrap();
+    let elph_valve = maze.get(elph).unwrap();
+    let mut res = 0;
+
+    for c in curr_valve.tunnels.iter() {
+        for e in elph_valve.tunnels.iter() {
+            res = max(res, elphsplore(memo, maze, time - 1, c, e, open));
+        }
+    }
+
+    if curr_valve.flow > 0 && !open.contains(curr) {
+        open.insert(curr.to_string());
+        for e in elph_valve.tunnels.iter() {
+            res = max(res, ((time - 1) * curr_valve.flow) + elphsplore(memo, maze, time - 1, curr, e, open));
+        }
+        open.remove(curr);
+    }
+
+    if elph_valve.flow > 0 && !open.contains(elph) {
+        open.insert(elph.to_string());
+        for c in curr_valve.tunnels.iter() {
+            res = max(res, ((time - 1) * elph_valve.flow) + elphsplore(memo, maze, time - 1, c, elph, open));
+        }
+        open.remove(elph);
+    }
+
+    if curr_valve.flow > 0 && !open.contains(curr) && elph_valve.flow > 0 && !open.contains(elph) {
+        open.insert(curr.to_string());
+        open.insert(elph.to_string());
+        res = max(res, ((time - 1) * curr_valve.flow) + ((time - 1) * elph_valve.flow) + elphsplore(memo, maze, time - 1, curr, elph, open));
+        open.remove(elph);
+        open.remove(curr);
+    }
+
+    memo.insert(
+        (
+            curr.to_string(),
+            elph.to_string(),
+            time,
+            open.iter().sorted().join(","),
+        ),
+        res,
+    );
 
     res
 }
 
 fn main() {
     let input = include_str!("../input");
-    println!("{:?}", part1(input));
+    println!("{}", part1(input));
+    println!("{}", part2(input));
 }
